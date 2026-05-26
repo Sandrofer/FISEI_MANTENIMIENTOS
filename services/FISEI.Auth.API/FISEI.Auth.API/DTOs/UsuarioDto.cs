81a963f53acc906d465c15f43d750bbcb78bc30e
ferdex <ferdex24@gmail.com>
2026-05-25 22:57:20 -0500

    Se agrego la correcta gestion de usuarios

--- services/FISEI.Auth.API/FISEI.Auth.API/DTOs/UsuarioDto.cs
+++ services/FISEI.Auth.API/FISEI.Auth.API/DTOs/UsuarioDto.cs
@@ -23,3 +23,12 @@ public class ActualizarRolDto
 {
     public string Rol { get; set; } = string.Empty;
 }
+
+public class ActualizarUsuarioDto
+{
+    public string Nombre { get; set; } = string.Empty;
+    public string Apellido { get; set; } = string.Empty;
+    public string Correo { get; set; } = string.Empty;
+    public string Rol { get; set; } = string.Empty;
+    public bool Activo { get; set; }
+}
